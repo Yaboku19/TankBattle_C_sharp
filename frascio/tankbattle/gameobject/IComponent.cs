9bd18a63240c92f27683e5c05d6edb230e7d10fd
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2023-04-13 18:25:34 +0200

    create IGameObject and IComponent

---
+++ frascio/tankbattle/gameobject/IComponent.cs
@@ -0,0 +1,7 @@
+namespace IComponent
+{
+    public class IComponent
+    {
+        
+    }
+}
---
+++ frascio/tankbattle/gameobject/IGameObject.cs
@@ -0,0 +1,12 @@
+namespace IGameObject
+{
+    public class IGameObject 
+    {
+        List<IComponent> Components {get ; }
+
+        void update();
+
+
+    }
+
+}
