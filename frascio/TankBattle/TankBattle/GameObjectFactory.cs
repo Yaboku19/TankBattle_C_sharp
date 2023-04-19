e90861cc15e4b52dbf08fb4e5927210d0ac534fc
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2023-04-19 18:37:46 +0200

    add GameObjectFactory

---
+++ frascio/TankBattle/TankBattle/GameObjectFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace TankBattle
+{
+    /// <summary>
+    /// Basic implementation of IGameObjectFactory.
+    /// </summary>
+    public class GameObjectFactory : IGameOjectFactory
+    {
+        private static readonly double SimpleTankDimension = 50;
+        private static readonly double SimpleBulletDimension = 10;
+        private static readonly double SimpleWallDimension = 40;
+
+        /// <inheritdoc />
+        public IGameObject CreateSimpleTank(Point2d pos, Player player) =>
+            new GameObject(new Transform(pos, Direction.UP, SimpleTankDimension, SimpleTankDimension))
+                .AddComponent(new Tank(player));
+
+        /// <inheritdoc />
+        public IGameObject CreateSimpleBullet(IGameObject tank) =>
+            new GameObject(new Transform(BulletPosition(tank), tank.Transform.Direction,
+                SimpleBulletDimension, SimpleBulletDimension));
+
+        /// <inheritdoc />
+        public IGameObject CreateSimpleWall(Point2d pos) =>
+            new GameObject(new Transform(pos, Direction.NONE, SimpleWallDimension, SimpleWallDimension));
+
+        private Point2d BulletPosition(IGameObject tank) =>
+            new Point2d(tank.Transform.Position.X +
+                tank.Transform.Direction.X * (tank.Transform.Length / 2 + SimpleBulletDimension),
+                tank.Transform.Position.Y +
+                tank.Transform.Direction.X * (tank.Transform.Length / 2 + SimpleBulletDimension));
+    }
+}
---
+++ frascio/TankBattle/TankBattle/IGameObjectFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace TankBattle
+{
+    /// <summary>
+    /// Represent the factory that creates all the different GameObject
+    /// </summary>
+    public interface IGameOjectFactory
+    {
+        /// <summary>
+        /// Create a new tank, recognizable by the component Tank.
+        /// </summary>
+        /// <param name="pos">Start position</param>
+        /// <param name="player">Player associated to the tank</param>
+        /// <returns>a new tank</returns>
+        IGameObject CreateSimpleTank(Point2d pos, Player player);
+
+        /// <summary>
+        /// Create a new bullet.
+        /// </summary>
+        /// <param name="tank">tank that shoot the bullet</param>
+        /// <returns>the new bullet</returns>
+        IGameObject CreateSimpleBullet(IGameObject tank);
+
+        /// <summary>
+        /// Create a new wall.
+        /// </summary>
+        /// <param name="pos">wall position</param>
+        /// <returns>a new wall</returns>
+        IGameObject CreateSimpleWall(Point2d pos);
+    }
+}
