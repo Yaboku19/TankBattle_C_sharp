74097a322fe41ba574cbfffc08b8c67d0b4375ab
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-04-19 15:10:32 +0200

    imported files from frascio folder

---
+++ martelli/tankbattle/gameobjectimportedbyfrascio/Direction.cs
@@ -0,0 +1,23 @@
+namespace gameobjectimportedbyfrascio
+{
+    public sealed class Direction
+    {
+        public double X {get; }
+        public double Y {get; }
+
+        public Point2d Vector => new Point2d(this.X, this.Y);
+
+
+        public static readonly Direction UP = new Direction(0, 1);
+        public static readonly Direction DOWN = new Direction(0, -1);
+        public static readonly Direction LEFT = new Direction(-1, 0);
+        public static readonly Direction RIGTH = new Direction(1, 0);
+        public static readonly Direction NONE = new Direction(0, 0);
+
+        public Direction(double X, int y)
+        {
+            this.X = X;
+            this.Y = y;
+        }
+    }
+}
---
+++ martelli/tankbattle/gameobjectimportedbyfrascio/Transform.cs
@@ -0,0 +1,18 @@
+namespace gameobjectimportedbyfrascio
+{
+    public class Transform
+    {
+        public Point2d Position { get; }
+        public Direction Direction{ get; }
+        public double Length{ get; }
+        public double Width{ get; }
+
+        public Transform(Point2d position, Direction direction, double length, double width) {
+            Position = position;
+            Direction = direction;
+            Length = length;
+            Width = width;
+        }
+    }
+    
+}
