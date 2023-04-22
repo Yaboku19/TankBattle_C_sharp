b14973b4c4375d94138a07f7d54985944ba0756a
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-04-22 16:30:52 +0200

    added space and remove some lines

--- martelli/tankbattle/gamestate/api/IGameState.cs
+++ martelli/tankbattle/gamestate/api/IGameState.cs
@@ -11,45 +11,8 @@ namespace gamestate.api
 
         IEnumerable<Transform> WallsTrasform {get; }
 
-        Transform GetTankTrasform(int player);
+        Transform? GetTankTrasform(int player);
 
         int GetTankLife(int player);
     }
 }
-/*
-public interface GameState extends CommandListener {
-
-    /**
-     * Creates a World with the following parameters.
-     * @param firstPlayer the first player
-     * @param secondPlayer the second player
-     * @param dataList the data for the map
-    void createWorld(Player firstPlayer, Player secondPlayer, MapData dataList);
-
-    /**
-     * Update the GameObject.
-     * @param time indicates the time passed after last update
-    void update(Double time);
-
-    /**
-     * Gets all the bullets {@link Transform}.
-     * @return a {@link Stream} of {@link Transform} of Bullets
-    Stream<Transform> getBulletsTrasform();
-
-    /**
-     * Gets all the walls {@link Transform}.
-     * @return a {@link Stream} of {@link Transform} of Walls
-    Stream<Transform> getWallsTrasform();
-
-    /**
-     * Gets tank {@link Transform} from the given {@link Player}.
-     * @param player used to identify the tank
-     * @return return the {@link Transform} of the tank
-    Transform getTankTrasform(Player player);
-
-    /**
-     * Return the life of the given {@link Player}.
-     * @param player used to identify the tank
-     * @return life points
-    int getTankLife(Player player);
-}*/
